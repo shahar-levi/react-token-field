6d9ab33d833542a32544738b0bbbfabd91804515
slevi <Maya@0810!>
2022-06-01 21:52:01 +0300

    Updates

---
+++ precache-manifest.60124482a9faa7ba9d1626087043930a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459882fcc7375d4e443294e63682c999",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34c0fa6af6f65e4c9924",
+    "url": "./static/css/main.4d0ab7ca.chunk.css"
+  },
+  {
+    "revision": "7b6ee32b9a9b6d306860",
+    "url": "./static/js/2.9d26adc9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9d26adc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c0fa6af6f65e4c9924",
+    "url": "./static/js/main.2f9f0810.chunk.js"
+  },
+  {
+    "revision": "765b9bc3be5b720d3293",
+    "url": "./static/js/runtime-main.22c57a44.js"
+  }
+]);
